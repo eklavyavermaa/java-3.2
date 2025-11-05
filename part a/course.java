554a5c061a48ce27eb848ff600292ce3e0381733
eklavyavermaa <168365984+eklavyavermaa@users.noreply.github.com>
2025-11-06 00:37:20 +0530

    Create course.java

---
+++ part a/course.java
@@ -0,0 +1,13 @@
+package com.example.di;
+
+public class Course {
+    private String courseName;
+
+    public Course() {
+        this.courseName = "Spring Framework with Java Configuration";
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
